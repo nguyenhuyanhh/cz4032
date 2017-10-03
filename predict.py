dc79942962710f77c2042f77f8af62c8348e7127
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-10-03 17:16:09 +0800

    WIP: bayesian optimization for hyper-params

--- predict.py
+++ predict.py
@@ -11,6 +11,7 @@ import os
 from time import time
 
 import numpy as np
+from bayes_opt import BayesianOptimization
 
 import xgboost as xgb
 
@@ -296,6 +297,7 @@ def preprocess():
     """
     Wrapper for preprocessing functions.
     """
+    print('preprocessing...')
     pre_train_path = os.path.join(MODEL_DIR, 'pre_train.csv')
     pre_test_path = os.path.join(MODEL_DIR, 'pre_test.csv')
     pre_bill_path = os.path.join(MODEL_DIR, 'pre_bill_of_materials.csv')
@@ -314,7 +316,8 @@ def preprocess():
     return merged_train_path, merged_test_path
 
 
-def train(features, train_set, config_file=os.path.join(CUR_DIR, 'config.json'), output_model=True):
+def train(features, train_set, config_file=os.path.join(CUR_DIR, 'config.json'),
+          output_model=True, bayes_opt=False):
     """
     Build the model for prediction.
 
@@ -326,6 +329,8 @@ def train(features, train_set, config_file=os.path.join(CUR_DIR, 'config.json'),
                 default is 'config.json' in current directory
         output_model: boolean - whether to output the model
                 default is True. if False, output cv score only
+        bayes_opt: boolean - whether to find best parameters using BO
+                default is False.
     """
     # get training matrix
     lines = list()
@@ -341,7 +346,7 @@ def train(features, train_set, config_file=os.path.join(CUR_DIR, 'config.json'),
             if i == 0:  # id
                 vects[i].append(int(values[i][-5:]))
             elif i == 25 or i == no_vects - 1:  # weight, cost
-                vects[i].append(math.log10(float(values[i]) + 1))
+                vects[i].append(math.log(float(values[i]) + 1))
             else:
                 vects[i].append(float(values[i]))
     a_mat = list()
@@ -356,14 +361,95 @@ def train(features, train_set, config_file=os.path.join(CUR_DIR, 'config.json'),
         params = json.load(cfg)
     num_round = 5000
 
-    # output model
-    if output_model:
-        model = xgb.train(params, dtrain, num_round)
-        model.save_model(os.path.join(MODEL_DIR, '0001.model'))
+    def xgb_evaluate(min_child_weight=None, subsample=None, colsample_bytree=None,
+                     scale_pos_weight=None, max_depth=None, max_delta_step=None,
+                     gamma=None):
+        """Target function to evaluate model, uses xgboost's cv method."""
+        # target params
+        if min_child_weight is not None:
+            params['min_child_weight'] = int(
+                min_child_weight)  # int values only
+        if subsample is not None:
+            params['subsample'] = max(min(subsample, 1), 0)  # between 0 and 1
+        if colsample_bytree is not None:
+            params['colsample_bytree'] = max(
+                min(colsample_bytree, 1), 0)  # between 0 and 1
+        if scale_pos_weight is not None:
+            params['scale_pos_weight'] = max(
+                min(scale_pos_weight, 1), 0)  # between 0 and 1
+        if max_depth is not None:
+            params['max_depth'] = int(max_depth)  # int values only
+        if max_delta_step is not None:
+            params['max_delta_step'] = int(max_delta_step)  # int values only
+        if gamma is not None:
+            params['gamma'] = max(gamma, 0) # must be positive
+
+        # other params
+        params['silent'] = 1  # silent output
+        params['eta'] = 0.1
+
+        # save params to temp
+        with open(os.path.join(CUR_DIR, 'config_bo.json'), 'w') as cfg:
+            json.dump(params, cfg, indent=4)
+
+        # perform cv
+        res = xgb.cv(params, dtrain, num_boost_round=1000, nfold=5, metrics={
+            'rmse'}, early_stopping_rounds=25, seed=0)
+        return -res['test-rmse-mean'].values[-1]
+
+    def xgb_bayes_opt():
+        """Perform Bayesian Optimization."""
+        print('performing hyper-parameter tuning...')
+
+        # target params
+        tests = {
+            'min_child_weight': (1, 20),
+            'subsample': (0.5, 1),
+            'colsample_bytree': (0.5, 1),
+            'max_depth': (6, 10),
+            'max_delta_step': (0, 10),
+            'gamma': (0, 10)
+        }
+
+        xgb_bo = BayesianOptimization(xgb_evaluate, tests)
+        xgb_bo.maximize(init_points=25, n_iter=10)
+
+        # get the optimised params
+        params_opt = xgb_bo.res['max']['max_params']
+        print(params_opt)
+        
+        # run one more time to lock the values to config_bo
+        xgb_bo.explore({k: [v] for k, v in params_opt.items()})
+
+    # operations
+    if bayes_opt:
+        if os.path.exists(os.path.join(CUR_DIR, 'config_bo.json')):
+            # BO performed before
+            print('performed hyper-parameter tuning previously')
+        else:
+            xgb_bayes_opt()
+
+        with open(os.path.join(CUR_DIR, 'config_bo.json'), 'r') as cfg:
+            params = json.load(cfg)
+        del params['silent'] # show output again
+
+        if output_model:
+            print('training...')
+            model = xgb.train(params, dtrain, num_round)
+            model.save_model(os.path.join(MODEL_DIR, '0001.model'))
+        else:
+            print('performing cross-validation...')
+            eval_ = -xgb_evaluate()
+            print('final test rmse: {}'.format(eval_))
     else:
-        # using the built in cv method to check errors, it uses rmse though
-        xgb.cv(params, dtrain, num_round, nfold=5, metrics={'rmse'}, seed=0, callbacks=[
-            xgb.callback.print_evaluation(show_stdv=True)])
+        if output_model:
+            print('training...')
+            model = xgb.train(params, dtrain, num_round)
+            model.save_model(os.path.join(MODEL_DIR, '0001.model'))
+        else:
+            print('performing cross-validation...')
+            eval_ = -xgb_evaluate()
+            print('final test rmse: {}'.format(eval_))
 
 
 def predict(features, test_set):
@@ -375,6 +461,8 @@ def predict(features, test_set):
                 features must match a header item in csv
         test_set: str - path to test set
     """
+    print('predicting...')
+
     # get test matrix
     lines = list()
     with open(test_set, 'r') as merged_:
@@ -389,7 +477,7 @@ def predict(features, test_set):
             if i == 0:  # id
                 vects[i].append(int(values[i][-5:]))
             elif i == 25:  # weight
-                vects[i].append(math.log10(float(values[i]) + 1))
+                vects[i].append(math.log(float(values[i]) + 1))
             else:
                 vects[i].append(float(values[i]))
     a_mat = list()
@@ -409,13 +497,13 @@ def predict(features, test_set):
     with open(OUT_FILE, 'w') as out_:
         out_.write('id,cost\n')
         for pred in ypred:
-            cost = math.pow(10, pred) - 1
-            # transform predictions back to y with (10 ** pred) - 1
+            # transform predictions back to y with exp(pred) - 1
+            cost = math.exp(pred) - 1
             out_.write('{},{}\n'.format(id_, cost))
             id_ += 1
 
 
-def main(retrain=False):
+def main(args):
     """Wrapper for everything."""
     # specify the feature order
     feats = ['tube_assembly_id', 'diameter', 'wall', 'length', 'num_bends',
@@ -429,14 +517,11 @@ def main(retrain=False):
              'min_order_quantity', 'bracket_pricing', 'quantity']
     # main
     start_time = time()
-    print('preprocessing...')
     train_set, test_set = preprocess()
-    if retrain:
-        print('training...')
-        train(feats, train_set)
-    print('predicting...')
-    predict(feats, test_set)
-    print('done')
+    if args.train:
+        train(feats, train_set, output_model=(not args.cv), bayes_opt=args.bo)
+    if args.predict:
+        predict(feats, test_set)
     end_time = time()
     print('exec time is {} seconds'.format(end_time - start_time))
 
@@ -444,6 +529,12 @@ def main(retrain=False):
 if __name__ == '__main__':
     ARG_PARSER = argparse.ArgumentParser()
     ARG_PARSER.add_argument(
-        '-r', '--retrain', action='store_true', help='retrain the model')
+        '-p', '--predict', action='store_true', help='do predictions')
+    ARG_PARSER.add_argument(
+        '-t', '--train', action='store_true', help='train the model')
+    ARG_PARSER.add_argument(
+        '-c', '--cv', action='store_true', help='do cross-validation (no model output)')
+    ARG_PARSER.add_argument(
+        '-b', '--bo', action='store_true', help='hyper-parameter tuning using BO')
     ARGS = ARG_PARSER.parse_args()
-    main(ARGS.retrain)
+    main(ARGS)
