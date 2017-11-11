a6de15fc5480917c76372cb3f81a8014a8b26864
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-11-11 22:33:45 +0800

    visualize specs

--- visualize.py
+++ visualize.py
@@ -11,7 +11,6 @@ import pandas as pd
 # define stuff here
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(CUR_DIR, 'competition_data')
-TRAIN_FILE = os.path.join(DATA_DIR, 'train_set.csv')
 VIZ_DIR = os.path.join(CUR_DIR, 'viz/')
 if not os.path.exists(VIZ_DIR):
     os.makedirs(VIZ_DIR)
@@ -20,7 +19,7 @@ if not os.path.exists(VIZ_DIR):
 def visualize_suppliers():
     """Visualize the distribution of suppliers."""
     # read training file
-    df_in = pd.read_csv(TRAIN_FILE)
+    df_in = pd.read_csv(os.path.join(DATA_DIR, 'train_set.csv'))
 
     # calculate distribution of suppliers
     supp_dist = Counter(df_in['supplier'])
@@ -38,12 +37,39 @@ def visualize_suppliers():
     # sort and plot
     values = sorted(new_supp_dist.values(), reverse=True)
     keys = sorted(new_supp_dist, key=new_supp_dist.get, reverse=True)
+    plt.figure()
     plt.bar(range(len(values)), values, align='center')
     plt.xticks(range(len(values)), keys, rotation=45)
     plt.suptitle('Distribution of Suppliers')
     plt.savefig(os.path.join(VIZ_DIR, 'visualize_suppliers.png'))
+
+
+def visualize_specs():
+    """Visualize the distribution of specs."""
+    # read specs file
+    df_in = pd.read_csv(os.path.join(DATA_DIR, 'specs.csv'))
+
+    # process spec counts
+    df_in['count'] = df_in[['spec{}'.format(
+        i + 1) for i in range(10)]].count(axis=1)
+    spec_dist = Counter(df_in['count'])
+
+    # sort and plot
+    keys = sorted(spec_dist)
+    values = [spec_dist[key] for key in keys]
+    plt.figure()
+    plt.bar(range(len(values)), values, align='center')
+    plt.xticks(range(len(values)), keys)
+    plt.suptitle('Distribution of Specs')
+    plt.savefig(os.path.join(VIZ_DIR, 'visualize_specs.png'))
+
+
+def visualize():
+    """Wrapper for all visualizations."""
+    visualize_suppliers()
+    visualize_specs()
     plt.show()
 
 
 if __name__ == '__main__':
-    visualize_suppliers()
+    visualize()
